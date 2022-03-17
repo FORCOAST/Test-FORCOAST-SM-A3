94ea9d5ae1b0f5b58ad3a61778cc491eb7b70928
danieltwigt <50665134+danieltwigt@users.noreply.github.com>
2022-03-17 15:30:15 +0100

    Update run.sh
    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ INITIAL_DIR="$(pwd)"
 cd /usr/src/app
 Rscript ./Site_selection_Limfjord_netcdf_V3_mam.R $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${16} ${17} ${18}
 python3 ./Bulletin/send_bulletin.py -A $1 -B $2 -C $3 -D $4 -E $5 -F $6 -G $7 -H $8 -I $9 -J ${10} -K ${11} -L ${12} -M ${13} -N ${14} -O ${15} 
+
+cp ${14} $INITIAL_DIR
